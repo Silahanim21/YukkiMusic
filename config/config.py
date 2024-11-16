81c70e8b54d83eea83203e0bfc6068c2f9328536
Silahanim21 <turkcell63632@gmail.com>
2024-11-16 16:54:21 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -204,12 +204,12 @@ autoclean = []
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg",  # This is the file id of the photo you can also put the url of photo
+    "https://envs.sh/Lx9.jpg",  # This is the file id of the photo you can also put the url of photo
 )
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/91533956c91d0fd7c9f20.jpg",
+    "https://envs.sh/Lx9.jpg",
 )
 
 PLAYLIST_IMG_URL = getenv(
